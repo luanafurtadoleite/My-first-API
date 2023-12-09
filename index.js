2c9f35cbc7349d5c6e865a46f67c7c212bf9b018
luanafurtadoleite <luanafurtadoleite@gmail.com>
2023-12-09 13:39:42 -0300

    projeto final para conexão

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const express = require('express');
-const uuid = require('uuid');
-const port = 3000;
+import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+import cors from 'cors';
+
+
+const port = 3001;
 const app = express();
-app.use(express.json())
+
+app.use(express.json());
+app.use(cors())
+
 
 const users = []
 
@@ -25,7 +31,7 @@ app.get('/users', (request, response) => {
 
 app.post('/users', (request, response) => {
     const {name, age} = request.body
-    const user = { id: uuid.v4(), name, age }
+    const user = { id: uuidv4(), name, age };
 
     users.push(user)
 
@@ -38,11 +44,12 @@ app.put('/users/:id', (request, response) => {
     const id = request.userId
 
 
-    const updateUser = { id, name, age }
+    const updateUser = { id: request.userId, name, age }
      
     users[index] = updateUser
 
-    return response.json(updateUser);
+    return response.json(users[index]);
+
 });
 
 app.delete('/users/:id', (request, response) => {
@@ -56,3 +63,6 @@ app.delete('/users/:id', (request, response) => {
 app.listen(port, () => {
     console.log(`O servidor está rodando na porta ${port}`);
 });
+
+
+app.use('/users/:id', checkUserId);
